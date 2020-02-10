874d6b73007918ea237181c8ef0a1c3d1bd6659c
bobalobdob <kyranperdue@gmail.com>
2020-02-10 00:05:47 -0800

    update

--- Burg's Burgers/Burg's Burgers/Form1.Designer.cs
+++ Burg's Burgers/Burg's Burgers/Form1.Designer.cs
@@ -28,86 +28,17 @@ namespace Burg_s_Burgers
         /// </summary>
         private void InitializeComponent()
         {
-            this.gBoxMenu = new System.Windows.Forms.GroupBox();
-            this.numUpDwnMenu1 = new System.Windows.Forms.NumericUpDown();
-            this.cBoxMenu1 = new System.Windows.Forms.CheckBox();
-            this.gBoxOrder = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnAddItem = new System.Windows.Forms.Button();
             this.pBoxLogo = new System.Windows.Forms.PictureBox();
             this.lblLogo = new System.Windows.Forms.Label();
-            this.lblPrice = new System.Windows.Forms.Label();
-            this.btnOrder = new System.Windows.Forms.Button();
-            this.lblOrder = new System.Windows.Forms.Label();
-            this.gBoxMenu.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numUpDwnMenu1)).BeginInit();
-            this.gBoxOrder.SuspendLayout();
+            this.btnCreateOrder = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxLogo)).BeginInit();
             this.SuspendLayout();
-            // 
-            // gBoxMenu
-            // 
-            this.gBoxMenu.Controls.Add(this.btnAddItem);
-            this.gBoxMenu.Controls.Add(this.label1);
-            this.gBoxMenu.Controls.Add(this.numUpDwnMenu1);
-            this.gBoxMenu.Controls.Add(this.cBoxMenu1);
-            this.gBoxMenu.Location = new System.Drawing.Point(12, 12);
-            this.gBoxMenu.Name = "gBoxMenu";
-            this.gBoxMenu.Size = new System.Drawing.Size(209, 215);
-            this.gBoxMenu.TabIndex = 0;
-            this.gBoxMenu.TabStop = false;
-            this.gBoxMenu.Text = "Menu";
-            // 
-            // numUpDwnMenu1
-            // 
-            this.numUpDwnMenu1.Location = new System.Drawing.Point(6, 42);
-            this.numUpDwnMenu1.Name = "numUpDwnMenu1";
-            this.numUpDwnMenu1.Size = new System.Drawing.Size(79, 20);
-            this.numUpDwnMenu1.TabIndex = 1;
-            // 
-            // cBoxMenu1
-            // 
-            this.cBoxMenu1.AutoSize = true;
-            this.cBoxMenu1.Location = new System.Drawing.Point(6, 19);
-            this.cBoxMenu1.Name = "cBoxMenu1";
-            this.cBoxMenu1.Size = new System.Drawing.Size(79, 17);
-            this.cBoxMenu1.TabIndex = 0;
-            this.cBoxMenu1.Text = "MenuItem1";
-            this.cBoxMenu1.UseVisualStyleBackColor = true;
-            // 
-            // gBoxOrder
-            // 
-            this.gBoxOrder.Controls.Add(this.lblOrder);
-            this.gBoxOrder.Controls.Add(this.btnOrder);
-            this.gBoxOrder.Controls.Add(this.lblPrice);
-            this.gBoxOrder.Location = new System.Drawing.Point(12, 233);
-            this.gBoxOrder.Name = "gBoxOrder";
-            this.gBoxOrder.Size = new System.Drawing.Size(415, 223);
-            this.gBoxOrder.TabIndex = 1;
-            this.gBoxOrder.TabStop = false;
-            this.gBoxOrder.Text = "Confirm Order";
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(91, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 42);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Item Description";
-            // 
-            // btnAddItem
-            // 
-            this.btnAddItem.Location = new System.Drawing.Point(6, 164);
-            this.btnAddItem.Name = "btnAddItem";
-            this.btnAddItem.Size = new System.Drawing.Size(197, 45);
-            this.btnAddItem.TabIndex = 3;
-            this.btnAddItem.Text = "Add Item";
-            this.btnAddItem.UseVisualStyleBackColor = true;
             // 
             // pBoxLogo
             // 
             this.pBoxLogo.Image = global::Burg_s_Burgers.Properties.Resources.Borger;
-            this.pBoxLogo.Location = new System.Drawing.Point(227, 12);
+            this.pBoxLogo.Location = new System.Drawing.Point(115, 12);
             this.pBoxLogo.Name = "pBoxLogo";
             this.pBoxLogo.Size = new System.Drawing.Size(200, 101);
             this.pBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -116,71 +47,52 @@ namespace Burg_s_Burgers
             // 
             // lblLogo
             // 
-            this.lblLogo.Location = new System.Drawing.Point(230, 124);
+            this.lblLogo.Location = new System.Drawing.Point(119, 116);
             this.lblLogo.Name = "lblLogo";
-            this.lblLogo.Size = new System.Drawing.Size(196, 97);
+            this.lblLogo.Size = new System.Drawing.Size(196, 81);
             this.lblLogo.TabIndex = 3;
             this.lblLogo.Text = "Description of Burg\'s Burgers\r\nBuy our burgers, bro\r\nthey got meat and cheese";
             // 
-            // lblPrice
+            // btnCreateOrder
             // 
-            this.lblPrice.Location = new System.Drawing.Point(215, 147);
-            this.lblPrice.Name = "lblPrice";
-            this.lblPrice.Size = new System.Drawing.Size(194, 73);
-            this.lblPrice.TabIndex = 0;
-            this.lblPrice.Text = "Price";
+            this.btnCreateOrder.Location = new System.Drawing.Point(12, 200);
+            this.btnCreateOrder.Name = "btnCreateOrder";
+            this.btnCreateOrder.Size = new System.Drawing.Size(170, 101);
+            this.btnCreateOrder.TabIndex = 4;
+            this.btnCreateOrder.Text = "Create Order";
+            this.btnCreateOrder.UseVisualStyleBackColor = true;
+            this.btnCreateOrder.Click += new System.EventHandler(this.btnCreateOrder_Click);
             // 
-            // btnOrder
+            // button2
             // 
-            this.btnOrder.Location = new System.Drawing.Point(0, 147);
-            this.btnOrder.Name = "btnOrder";
-            this.btnOrder.Size = new System.Drawing.Size(209, 70);
-            this.btnOrder.TabIndex = 1;
-            this.btnOrder.Text = "Place Order";
-            this.btnOrder.UseVisualStyleBackColor = true;
-            // 
-            // lblOrder
-            // 
-            this.lblOrder.AutoSize = true;
-            this.lblOrder.Location = new System.Drawing.Point(6, 16);
-            this.lblOrder.Name = "lblOrder";
-            this.lblOrder.Size = new System.Drawing.Size(84, 13);
-            this.lblOrder.TabIndex = 2;
-            this.lblOrder.Text = "Your Order Here";
+            this.button2.Location = new System.Drawing.Point(253, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 101);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "View Orders";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(435, 468);
+            this.ClientSize = new System.Drawing.Size(435, 313);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCreateOrder);
             this.Controls.Add(this.lblLogo);
             this.Controls.Add(this.pBoxLogo);
-            this.Controls.Add(this.gBoxOrder);
-            this.Controls.Add(this.gBoxMenu);
             this.Name = "Form1";
             this.Text = "Burg\'s Burgers";
-            this.gBoxMenu.ResumeLayout(false);
-            this.gBoxMenu.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numUpDwnMenu1)).EndInit();
-            this.gBoxOrder.ResumeLayout(false);
-            this.gBoxOrder.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pBoxLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox gBoxMenu;
-        private System.Windows.Forms.NumericUpDown numUpDwnMenu1;
-        private System.Windows.Forms.CheckBox cBoxMenu1;
-        private System.Windows.Forms.GroupBox gBoxOrder;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnAddItem;
         private System.Windows.Forms.PictureBox pBoxLogo;
         private System.Windows.Forms.Label lblLogo;
-        private System.Windows.Forms.Label lblOrder;
-        private System.Windows.Forms.Button btnOrder;
-        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Button btnCreateOrder;
+        private System.Windows.Forms.Button button2;
     }
 }
---
+++ Burg's Burgers/Burg's Burgers/Form2.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Burg_s_Burgers
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numUpDwnBurgers = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDwnBurgers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numUpDwnBurgers);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(363, 333);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Place Order";
+            // 
+            // numUpDwnBurgers
+            // 
+            this.numUpDwnBurgers.Location = new System.Drawing.Point(6, 56);
+            this.numUpDwnBurgers.Name = "numUpDwnBurgers";
+            this.numUpDwnBurgers.Size = new System.Drawing.Size(120, 20);
+            this.numUpDwnBurgers.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 228);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(6, 307);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(121, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(6, 281);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(121, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 255);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(137, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 176);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(134, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "City";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(134, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "State";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(134, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Phone Number";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(183, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Select the number of Burgers to order";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 357);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form2";
+            this.Text = "Place Order";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDwnBurgers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numUpDwnBurgers;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
