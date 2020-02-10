94e253ce04f0668fbacf53d427c506f9afb528b3
bobalobdob <kyranperdue@gmail.com>
2020-02-10 00:17:13 -0800

    ui done

--- Burg's Burgers/Burg's Burgers/Form2.Designer.cs
+++ Burg's Burgers/Burg's Burgers/Form2.Designer.cs
@@ -30,39 +30,41 @@
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.numUpDwnBurgers = new System.Windows.Forms.NumericUpDown();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.cBoxState = new System.Windows.Forms.ComboBox();
+            this.tBoxPhone = new System.Windows.Forms.TextBox();
+            this.tBoxAddress = new System.Windows.Forms.TextBox();
+            this.tBoxCity = new System.Windows.Forms.TextBox();
+            this.tBoxLname = new System.Windows.Forms.TextBox();
+            this.tBoxFname = new System.Windows.Forms.TextBox();
+            this.lblFname = new System.Windows.Forms.Label();
+            this.lblLname = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblBurger = new System.Windows.Forms.Label();
+            this.btnPlaceOrder = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numUpDwnBurgers)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnPlaceOrder);
+            this.groupBox1.Controls.Add(this.lblBurger);
+            this.groupBox1.Controls.Add(this.lblPhone);
+            this.groupBox1.Controls.Add(this.lblState);
+            this.groupBox1.Controls.Add(this.lblCity);
+            this.groupBox1.Controls.Add(this.lblAddress);
+            this.groupBox1.Controls.Add(this.lblLname);
+            this.groupBox1.Controls.Add(this.lblFname);
             this.groupBox1.Controls.Add(this.numUpDwnBurgers);
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Controls.Add(this.textBox6);
-            this.groupBox1.Controls.Add(this.textBox5);
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.cBoxState);
+            this.groupBox1.Controls.Add(this.tBoxPhone);
+            this.groupBox1.Controls.Add(this.tBoxAddress);
+            this.groupBox1.Controls.Add(this.tBoxCity);
+            this.groupBox1.Controls.Add(this.tBoxLname);
+            this.groupBox1.Controls.Add(this.tBoxFname);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(363, 333);
@@ -77,111 +79,121 @@
             this.numUpDwnBurgers.Size = new System.Drawing.Size(120, 20);
             this.numUpDwnBurgers.TabIndex = 7;
             // 
-            // comboBox1
+            // cBoxState
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(6, 228);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 6;
+            this.cBoxState.FormattingEnabled = true;
+            this.cBoxState.Location = new System.Drawing.Point(6, 280);
+            this.cBoxState.Name = "cBoxState";
+            this.cBoxState.Size = new System.Drawing.Size(121, 21);
+            this.cBoxState.TabIndex = 6;
             // 
-            // textBox6
+            // tBoxPhone
             // 
-            this.textBox6.Location = new System.Drawing.Point(6, 307);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(121, 20);
-            this.textBox6.TabIndex = 5;
+            this.tBoxPhone.Location = new System.Drawing.Point(6, 307);
+            this.tBoxPhone.Name = "tBoxPhone";
+            this.tBoxPhone.Size = new System.Drawing.Size(121, 20);
+            this.tBoxPhone.TabIndex = 5;
             // 
-            // textBox5
+            // tBoxAddress
             // 
-            this.textBox5.Location = new System.Drawing.Point(6, 281);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(121, 20);
-            this.textBox5.TabIndex = 4;
+            this.tBoxAddress.Location = new System.Drawing.Point(6, 228);
+            this.tBoxAddress.Name = "tBoxAddress";
+            this.tBoxAddress.Size = new System.Drawing.Size(121, 20);
+            this.tBoxAddress.TabIndex = 4;
             // 
-            // textBox4
+            // tBoxCity
             // 
-            this.textBox4.Location = new System.Drawing.Point(6, 255);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(121, 20);
-            this.textBox4.TabIndex = 3;
+            this.tBoxCity.Location = new System.Drawing.Point(6, 254);
+            this.tBoxCity.Name = "tBoxCity";
+            this.tBoxCity.Size = new System.Drawing.Size(121, 20);
+            this.tBoxCity.TabIndex = 3;
             // 
-            // textBox2
+            // tBoxLname
             // 
-            this.textBox2.Location = new System.Drawing.Point(137, 176);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 20);
-            this.textBox2.TabIndex = 1;
+            this.tBoxLname.Location = new System.Drawing.Point(137, 176);
+            this.tBoxLname.Name = "tBoxLname";
+            this.tBoxLname.Size = new System.Drawing.Size(121, 20);
+            this.tBoxLname.TabIndex = 1;
             // 
-            // textBox1
+            // tBoxFname
             // 
-            this.textBox1.Location = new System.Drawing.Point(6, 176);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
-            this.textBox1.TabIndex = 0;
+            this.tBoxFname.Location = new System.Drawing.Point(6, 176);
+            this.tBoxFname.Name = "tBoxFname";
+            this.tBoxFname.Size = new System.Drawing.Size(121, 20);
+            this.tBoxFname.TabIndex = 0;
             // 
-            // label1
+            // lblFname
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 160);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "First Name";
+            this.lblFname.AutoSize = true;
+            this.lblFname.Location = new System.Drawing.Point(6, 160);
+            this.lblFname.Name = "lblFname";
+            this.lblFname.Size = new System.Drawing.Size(57, 13);
+            this.lblFname.TabIndex = 8;
+            this.lblFname.Text = "First Name";
             // 
-            // label2
+            // lblLname
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(134, 160);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Last Name";
+            this.lblLname.AutoSize = true;
+            this.lblLname.Location = new System.Drawing.Point(134, 160);
+            this.lblLname.Name = "lblLname";
+            this.lblLname.Size = new System.Drawing.Size(58, 13);
+            this.lblLname.TabIndex = 9;
+            this.lblLname.Text = "Last Name";
             // 
-            // label3
+            // lblAddress
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(134, 228);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 13);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Address";
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(134, 228);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 10;
+            this.lblAddress.Text = "Address";
             // 
-            // label4
+            // lblCity
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(134, 255);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(24, 13);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "City";
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(134, 255);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(24, 13);
+            this.lblCity.TabIndex = 11;
+            this.lblCity.Text = "City";
             // 
-            // label5
+            // lblState
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(134, 281);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(32, 13);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "State";
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(134, 281);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(32, 13);
+            this.lblState.TabIndex = 12;
+            this.lblState.Text = "State";
             // 
-            // label6
+            // lblPhone
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(134, 307);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(78, 13);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "Phone Number";
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(134, 307);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(78, 13);
+            this.lblPhone.TabIndex = 13;
+            this.lblPhone.Text = "Phone Number";
             // 
-            // label7
+            // lblBurger
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 40);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(183, 13);
-            this.label7.TabIndex = 14;
-            this.label7.Text = "Select the number of Burgers to order";
+            this.lblBurger.AutoSize = true;
+            this.lblBurger.Location = new System.Drawing.Point(6, 40);
+            this.lblBurger.Name = "lblBurger";
+            this.lblBurger.Size = new System.Drawing.Size(183, 13);
+            this.lblBurger.TabIndex = 14;
+            this.lblBurger.Text = "Select the number of Burgers to order";
+            // 
+            // btnPlaceOrder
+            // 
+            this.btnPlaceOrder.Location = new System.Drawing.Point(218, 281);
+            this.btnPlaceOrder.Name = "btnPlaceOrder";
+            this.btnPlaceOrder.Size = new System.Drawing.Size(139, 46);
+            this.btnPlaceOrder.TabIndex = 15;
+            this.btnPlaceOrder.Text = "Place Order";
+            this.btnPlaceOrder.UseVisualStyleBackColor = true;
+            this.btnPlaceOrder.Click += new System.EventHandler(this.btnPlaceOrder_Click);
             // 
             // Form2
             // 
@@ -202,18 +214,19 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.NumericUpDown numUpDwnBurgers;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cBoxState;
+        private System.Windows.Forms.TextBox tBoxPhone;
+        private System.Windows.Forms.TextBox tBoxAddress;
+        private System.Windows.Forms.TextBox tBoxCity;
+        private System.Windows.Forms.TextBox tBoxLname;
+        private System.Windows.Forms.TextBox tBoxFname;
+        private System.Windows.Forms.Label lblBurger;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblLname;
+        private System.Windows.Forms.Label lblFname;
+        private System.Windows.Forms.Button btnPlaceOrder;
     }
 }
--- Burg's Burgers/Burg's Burgers/Form2.cs
+++ Burg's Burgers/Burg's Burgers/Form2.cs
@@ -16,5 +16,10 @@ namespace Burg_s_Burgers
         {
             InitializeComponent();
         }
+
+        private void btnPlaceOrder_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
