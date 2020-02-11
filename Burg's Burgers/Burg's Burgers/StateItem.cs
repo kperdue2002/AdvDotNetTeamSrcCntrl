d5ac670b2203fdc0fb0265555b3f3ed249066a26
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-02-10 16:36:51 -0800

    Added a Multi-Dimensional array containing the state names and their abbreviations.

---
+++ Burg's Burgers/Burg's Burgers/StateItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burg_s_Burgers
+{
+    public class StateItem
+    {
+        public string FullName { get; set; }
+        public string Abbreviation { get; set; }
+
+        public override string ToString()
+        {
+            return FullName;
+        }
+    }
+}
