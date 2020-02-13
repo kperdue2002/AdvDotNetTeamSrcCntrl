fe8715ca46e521ca923eeadf2cc4bf67768090dd
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-02-13 12:51:32 -0800

    The Place Order form is now functional. Proper form validation still needs to be added though.

---
+++ Burg's Burgers/Burg's Burgers/Migrations/202002132027422_Update-StringLimits.cs
@@ -0,0 +1,26 @@
+﻿namespace Burg_s_Burgers.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateStringLimits : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Orders", "Address", c => c.String(nullable: false, maxLength: 64));
+            AlterColumn("dbo.Orders", "City", c => c.String(nullable: false, maxLength: 32));
+            AlterColumn("dbo.Orders", "State", c => c.String(maxLength: 2, unicode: false));
+            AlterColumn("dbo.Orders", "ZipCode", c => c.String(nullable: false, maxLength: 5, unicode: false));
+            AlterColumn("dbo.Orders", "PhoneNumber", c => c.String(maxLength: 16, unicode: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Orders", "PhoneNumber", c => c.String());
+            AlterColumn("dbo.Orders", "ZipCode", c => c.String(nullable: false, maxLength: 8000, unicode: false));
+            AlterColumn("dbo.Orders", "State", c => c.String(maxLength: 8000, unicode: false));
+            AlterColumn("dbo.Orders", "City", c => c.String(nullable: false));
+            AlterColumn("dbo.Orders", "Address", c => c.String(nullable: false));
+        }
+    }
+}
--- Burg's Burgers/Burg's Burgers/Order.cs
+++ Burg's Burgers/Burg's Burgers/Order.cs
@@ -15,47 +15,61 @@ namespace Burg_s_Burgers
         public int OrderID { get; set; }
 
         [Required(AllowEmptyStrings = false)]
-        [StringLength(50)]
         [Column(TypeName = "varchar")]
+        [StringLength(50, ErrorMessage = "The first name input is too long.\n" +
+                                         "The maximimum input is 50 characters.")]
         public string FirstName { get; set; }
 
         [Required(AllowEmptyStrings = false)]
-        [StringLength(50)]
         [Column(TypeName = "varchar")]
+        [StringLength(50, ErrorMessage = "The last name input is too long.\n" +
+                                         "The maximimum input is 50 characters.")]
         public string LastName { get; set; }
         
         // Start of Address Section
         
         [Required(AllowEmptyStrings = false)]
+        [StringLength(64, ErrorMessage = "The address input is too long.\n" +
+                                         "The maximimum input is 64 characters.")]
         public string Address { get; set; }
 
         [Required(AllowEmptyStrings = false)]
         [RegularExpression(@"^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$",
          ErrorMessage = "The input for the city is not valid.")]
+        [StringLength(32, ErrorMessage = "The city input is too long.\n" +
+                                         "The maximimum input is 32 characters.")]
         public string City { get; set; }
 
+        [Required]
         [RegularExpression(@"^(?:(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|P[AR]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]))$",
          ErrorMessage = "The input for a state needs to be the capitalized abbreviation.")]
         [Column(TypeName = "varchar")]
+        [StringLength(2, ErrorMessage = "The state input is not abbreviated.\n" +
+                                        "Please file this as an error to the developers.")]
         public string State { get; set; }
 
         [Required(AllowEmptyStrings = false)]
         [RegularExpression(@"^\d{5}$",
-         ErrorMessage = "The zip code must be entirely made up of exactly 5 digits.")]
+         ErrorMessage = "The zip code must be made up of exactly 5 digits.")]
+        [StringLength(5, ErrorMessage = "The zip code must be made up of exactly 5 digits.")]
         [Column(TypeName = "varchar")]
         public string ZipCode { get; set; }
 
         // End of Address Section
 
         [Phone]
+        [Column(TypeName = "varchar")]
+        [StringLength(16, ErrorMessage = "The phone number input is too long.\n" +
+                                         "The maximimum input is 16 characters.")]
         public string PhoneNumber { get; set; }
 
         [Required]
         [Range(1, 250)]
         public byte Quantity { get; set; }
 
-        [StringLength(512)]
         [Column(TypeName = "nvarchar")]
+        [StringLength(512, ErrorMessage = "The special directions input is too long.\n" +
+                                          "The maximimum input is 512 characters.")]
         public string SpecialDirections { get; set; }
 
         [Required]
