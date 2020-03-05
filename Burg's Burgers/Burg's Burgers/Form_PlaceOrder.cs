4c56c9bc693199f7f858a6ce11cb823c055e8946
Kyran P <kyranperdue@gmail.com>
2020-03-05 13:05:01 -0800

    Initial Commit. Code checks if right number if fields are populated. will be changed to required fields. Throws exception if number isnt met (unhandled)

--- Burg's Burgers/Burg's Burgers/Form_PlaceOrder.cs
+++ Burg's Burgers/Burg's Burgers/Form_PlaceOrder.cs
@@ -19,21 +19,40 @@ namespace Burg_s_Burgers
 
         private void BtnPlaceOrder_Click(object sender, EventArgs e)
         {
-            Order newOrder = new Order
+            int numPopulated = 0;
+            foreach(Control c in Controls)
             {
-                FirstName = tBoxFname.Text,
-                LastName = tBoxLname.Text,
-                Address = tBoxAddress.Text,
-                City = tBoxCity.Text,
-                State = cBoxState.Text,
-                ZipCode = tBoxZip.Text,
-                PhoneNumber = tBoxPhone.Text,
-                Quantity = Convert.ToByte(numUpDwnBurgers.Value),
-                SpecialDirections = txtBoxInstruct.Text,
-                DateOfOrder = DateTime.Now
-            };
+                if (c is TextBox)
+                {
+                    TextBox currField = c as TextBox;
+                    if (!string.IsNullOrEmpty(currField.Text))
+                    {
+                        numPopulated++;
+                    }
+                }
+            }
+            if (numPopulated >= 6)
+            {
+                Order newOrder = new Order
+                {
+                    FirstName = tBoxFname.Text,
+                    LastName = tBoxLname.Text,
+                    Address = tBoxAddress.Text,
+                    City = tBoxCity.Text,
+                    State = cBoxState.Text,
+                    ZipCode = tBoxZip.Text,
+                    PhoneNumber = tBoxPhone.Text,
+                    Quantity = Convert.ToByte(numUpDwnBurgers.Value),
+                    SpecialDirections = txtBoxInstruct.Text,
+                    DateOfOrder = DateTime.Now
+                };
 
-            MessageBox.Show("Order Added");
+                MessageBox.Show("Order Added");
+            }
+            else
+            {
+                throw new Exception("All required fields must be populated.");
+            }
         }
     }
 }
